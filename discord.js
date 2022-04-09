3c2edc3620aee128ceeb1f9b0265f5abe0dbc903
zvbt <61088560+zvbt@users.noreply.github.com>
2022-04-09 10:07:48 +0200

    update

--- discord.js
+++ discord.js
@@ -10,36 +10,16 @@ discordClient.on("connected", () => {
 });
 
 module.exports = function discord(mainWindow) {
-
+  mainWindow.webContents.on('dom-ready', () => {
     let title = mainWindow.getTitle()
 
-  if (title.match("AnimeClient")) {
-    discordClient.updatePresence({
-      details: "Page d'acceuil",
-      state: "v1.1.2",
-      largeImageKey: 'logo',
-      largeImageText: 'AnimeClient v1.1.3',
-      smallImageKey: 'none',
-      instance: false,
-      buttons: [
-        {
-            "label": "Télécharger l'app",
-            "url": "https://zvbt.github.io/"
-        }
-    ]
-    });
-  }
-
-    //crunchy
-    const cr_site = title.split(' - ').pop();
-    const cr_episode = title.split(' - ').shift();
-    if (title.match("VOSTFR")) {
+    if (title.match("AnimeClient")) {
       discordClient.updatePresence({
-        details: "Crunchyroll",
-        state: "Page d'acceuil",
-        largeImageKey: 'crunchy',
-        largeImageText: 'Crunchyroll',
-        smallImageKey: 'logo',
+        details: "Page d'acceuil",
+        state: "v1.1.3",
+        largeImageKey: 'logo',
+        largeImageText: 'AnimeClient v1.1.3',
+        smallImageKey: 'none',
         instance: false,
         buttons: [
           {
@@ -49,79 +29,56 @@ module.exports = function discord(mainWindow) {
       ]
       });
     }
-    if (title.match("Regardez")) {
+  
+      //crunchy
+      const cr_site = title.split(' - ').pop();
+      const cr_episode = title.split(' - ').shift();
+      if (title.match("Crunchyroll")) {
+        discordClient.updatePresence({
+          details: "Crunchyroll",
+          state: "Page d'acceuil",
+          largeImageKey: 'crunchy',
+          largeImageText: 'Crunchyroll',
+          smallImageKey: 'logo',
+          instance: false,
+          buttons: [
+            {
+                "label": "Télécharger l'app",
+                "url": "https://zvbt.github.io/"
+            }
+        ]
+        });
+      }
+      if (title.match("Regardez sur") || title.match("Watch on") || title.match("Visionner sur")) {
+        discordClient.updatePresence({
+          details: "Crunchyroll",
+          state: cr_episode,
+          largeImageKey: 'crunchy',
+          largeImageText: 'Crunchyroll',
+          smallImageKey: 'logo',
+          instance: false,
+          buttons: [
+            {
+              "label": "Regarder l'épisode",
+              "url": mainWindow.webContents.getURL()
+            },
+            {
+                "label": "Télécharger l'app",
+                "url": "https://zvbt.github.io/"
+            }
+        ]
+        });
+      }
+  
+    //neko sama
+    const neko_site = title.split(' - ').pop();
+    const neko_episode = title.split(' - ').shift();
+    if (title.match("Infinité")) {
       discordClient.updatePresence({
-        details: "Crunchyroll",
-        state: cr_episode,
-        largeImageKey: 'crunchy',
-        largeImageText: 'Crunchyroll',
-        smallImageKey: 'logo',
-        instance: false,
-        buttons: [
-          {
-            "label": "Regarder l'épisode",
-            "url": mainWindow.webContents.getURL()
-          },
-          {
-              "label": "Télécharger l'app",
-              "url": "https://zvbt.github.io/"
-          }
-      ]
-      });
-    }
-
-  //neko sama
-  const neko_site = title.split(' - ').pop();
-  const neko_episode = title.split(' - ').shift();
-  if (title.match("Infinité")) {
-    discordClient.updatePresence({
-      details: "Neko-sama",
-      state: "Page d'acceuil",
-      largeImageKey: 'nekosama',
-      largeImageText: 'Neko-sama',
-      smallImageKey: 'logo',
-      instance: false,
-      buttons: [
-        {
-            "label": "Télécharger l'app",
-            "url": "https://zvbt.github.io/"
-        } 
-    ]
-    });
-  }
-  if (neko_site.match("sama")) {
-    discordClient.updatePresence({
-      details: neko_site,
-      state: neko_episode,
-      largeImageKey: 'nekosama',
-      largeImageText: 'Neko-sama',
-      smallImageKey: 'logo',
-      instance: false,
-      buttons: [
-        {
-          "label": "Regarder l'épisode",
-          "url": mainWindow.webContents.getURL()
-        },
-        {
-            "label": "Télécharger l'app",
-            "url": "https://zvbt.github.io/"
-        } 
-    ]
-    });
-  }
-
-    //voiranime
-    const va_site = title.split(' - ').pop();
-    const va_ep = title.split(' VOSTFR ').shift();
-    const va_ep1 = va_ep.split('- ').pop();
-    const va_episode = va_ep.split('- ').shift();
-
-    if (title.match("Voiranime")) {
-      discordClient.updatePresence({
-        details: "Voiranime",
+        details: "Neko-sama",
         state: "Page d'acceuil",
-        largeImageKey: 'va',
-        largeImageText: 'Voiranime',
+        largeImageKey: 'nekosama',
+        largeImageText: 'Neko-sama',
         smallImageKey: 'logo',
         instance: false,
         buttons: [
@@ -131,14 +88,98 @@ module.exports = function discord(mainWindow) {
           } 
       ]
       });
-  }
-
-    if (va_site.match("Voiranime")) {
+    }
+    if (neko_site.match("sama")) {
       discordClient.updatePresence({
-        details: va_site,
-        state: va_episode + " E" + va_ep1,
-        largeImageKey: 'va',
-        largeImageText: 'Voiranime',
+        details: neko_site,
+        state: neko_episode,
+        largeImageKey: 'nekosama',
+        largeImageText: 'Neko-sama',
+        smallImageKey: 'logo',
+        instance: false,
+        buttons: [
+          {
+            "label": "Regarder l'épisode",
+            "url": mainWindow.webContents.getURL()
+          },
+          {
+              "label": "Télécharger l'app",
+              "url": "https://zvbt.github.io/"
+          } 
+      ]
+      });
+    }
+  
+      //voiranime
+      const va_site = title.split(' - ').pop();
+      const va_ep = title.split(' VOSTFR ').shift();
+      const va_ep1 = va_ep.split('- ').pop();
+      const va_episode = va_ep.split('- ').shift();
+  
+      if (title.match("Voiranime")) {
+        discordClient.updatePresence({
+          details: "Voiranime",
+          state: "Page d'acceuil",
+          largeImageKey: 'va',
+          largeImageText: 'Voiranime',
+          smallImageKey: 'logo',
+          instance: false,
+          buttons: [
+            {
+                "label": "Télécharger l'app",
+                "url": "https://zvbt.github.io/"
+            } 
+        ]
+        });
+    }
+  
+      if (va_site.match("Voiranime")) {
+        discordClient.updatePresence({
+          details: va_site,
+          state: va_episode + " E" + va_ep1,
+          largeImageKey: 'va',
+          largeImageText: 'Voiranime',
+          smallImageKey: 'logo',
+          instance: false,
+          buttons: [
+            {
+              "label": "Regarder l'épisode",
+              "url": mainWindow.webContents.getURL()
+            },
+            {
+                "label": "Télécharger l'app",
+                "url": "https://zvbt.github.io/"
+            }
+        ]
+        });
+      }
+   
+  
+    //vostfree
+    const vf_episode = title.split(' en DDL').shift();
+  
+    if (title.match("Gratuit")) {
+      discordClient.updatePresence({
+        details: "Vostfree",
+        state: "Page d'acceuil",
+        largeImageKey: 'vost',
+        largeImageText: 'Vostfree',
+        smallImageKey: 'logo',
+        instance: false,
+        buttons: [
+          {
+              "label": "Télécharger l'app",
+              "url": "https://zvbt.github.io/"
+          }
+      ]
+      });
+    }
+    if (title.match("DDL")) {
+      discordClient.updatePresence({
+        details: "VostFree",
+        state: vf_episode,
+        largeImageKey: 'vost',
+        largeImageText: 'Vostfree',
         smallImageKey: 'logo',
         instance: false,
         buttons: [
@@ -153,65 +194,9 @@ module.exports = function discord(mainWindow) {
       ]
       });
     }
- 
-
-  //vostfree
-  const vf_episode = title.split(' en DDL').shift();
-
-  if (title.match("Gratuit")) {
-    discordClient.updatePresence({
-      details: "Vostfree",
-      state: "Page d'acceuil",
-      largeImageKey: 'vost',
-      largeImageText: 'Vostfree',
-      smallImageKey: 'logo',
-      instance: false,
-      buttons: [
-        {
-            "label": "Télécharger l'app",
-            "url": "https://zvbt.github.io/"
-        }
-    ]
-    });
-  }
-  if (title.match("DDL")) {
-    discordClient.updatePresence({
-      details: "VostFree",
-      state: vf_episode,
-      largeImageKey: 'vost',
-      largeImageText: 'Vostfree',
-      smallImageKey: 'logo',
-      instance: false,
-      buttons: [
-        {
-          "label": "Regarder l'épisode",
-          "url": mainWindow.webContents.getURL()
-        },
-        {
-            "label": "Télécharger l'app",
-            "url": "https://zvbt.github.io/"
-        }
-    ]
-    });
-  }
-
-//anilist
-if (title.match("AniList")) {
-  discordClient.updatePresence({
-    details: title,
-    state: "    ",
-    largeImageKey: 'al',
-    smallImageKey: 'logo',
-    instance: false,
-    buttons: [
-      {
-          "label": "Télécharger l'app",
-          "url": "https://zvbt.github.io/"
-      }
-  ]
-  });
-  }
-  if (title.match("'s ")) {
+  
+  //anilist
+  if (title.match("AniList")) {
     discordClient.updatePresence({
       details: title,
       state: "    ",
@@ -220,14 +205,31 @@ if (title.match("AniList")) {
       instance: false,
       buttons: [
         {
-          "label": "Voir le profile",
-          "url": mainWindow.webContents.getURL()
-        },
-        {
             "label": "Télécharger l'app",
             "url": "https://zvbt.github.io/"
         }
     ]
     });
-  }
+    }
+    if (title.match("'s ")) {
+      discordClient.updatePresence({
+        details: title,
+        state: "    ",
+        largeImageKey: 'al',
+        smallImageKey: 'logo',
+        instance: false,
+        buttons: [
+          {
+            "label": "Voir le profile",
+            "url": mainWindow.webContents.getURL()
+          },
+          {
+              "label": "Télécharger l'app",
+              "url": "https://zvbt.github.io/"
+          }
+      ]
+      });
+    }
+})
+    
 };
