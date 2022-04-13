34ff2402dd838ff3e78721ee6b77a9fade8dacaf
zvbt <zvbt@pm.me>
2022-04-13 22:46:18 +0200

    updated to 1.1.5

--- client/image.js
+++
@@ -1,6 +0,0 @@
-function getRandomImage() {
-    let aaa = Math.floor(Math.random() * 120) + 1;
-    let imgUrl = 'background-image: url("https://raw.githubusercontent.com/zvbt/animeclient-assets/main/assets/' + aaa + '.png")'
-    var imgStr = "<canvas class='bg' style='" + imgUrl + "'></canvas>";
-    document.write(imgStr); document.close();
-}
