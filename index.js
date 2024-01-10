5f781edec958afb271cb3226a0f58b3865a7697d
zvbt <zvbt@duck.com>
2024-01-10 01:20:43 +0100

    auto update using electron-updater

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-const { electron, app, BrowserWindow, ipcMain, Menu } = require("electron");
+const { electron, app, BrowserWindow, ipcMain, Menu, dialog } = require("electron");
 const { fetch } = require("cross-fetch");
 const path = require("path");
+const { autoUpdater } = require("electron-updater");
 const { readFileSync, writeFileSync } = require("fs");
 const {
   ElectronBlocker,
@@ -143,6 +144,8 @@ async function createWindow() {
       document.head.appendChild(style);
     `);
     }
+
+    autoUpdater.checkForUpdates();
   });
 
   // //javascript injection
@@ -160,7 +163,37 @@ async function createWindow() {
   });
 
   mainWindow.setIcon("./build/logo.ico");
+
+
+
 }
+
+// updater
+autoUpdater.on("update-available", (_event, releaseNotes, releaseName) =>{
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Ok'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version is being downloaded.'
+  }
+  dialog.showMessageBox(dialogOpts, (response) => {
+
+  })
+})
+
+autoUpdater.on("update-downloaded", (_event, releaseNotes, releaseName) =>{
+  const dialogOpts = {
+    type: 'info',
+    buttons: ['Restart', 'Later'],
+    title: 'Application Update',
+    message: process.platform === 'win32' ? releaseNotes : releaseName,
+    detail: 'A new version has been downloaded> Restart the application to apply the updates.'
+  }
+  dialog.showMessageBox(dialogOpts).then((returnValue) =>{
+    if (returnValue.response === 0) autoUpdater.quitAndInstall()
+  })
+})
 
 app.on("ready", () => {
   createWindow();
@@ -180,6 +213,7 @@ app.on("activate", () => {
 });
 
 const InfiniteLoop = require("infinite-loop");
+const { release } = require("os");
 let il = new InfiniteLoop();
 function discordrpc() {
   discord(mainWindow);
